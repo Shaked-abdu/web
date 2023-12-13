c2aeab78f2bddee8a71cad7c5f6f059f74c393d0
unknown <shakedabdu50@gmail.com>
2023-12-13 09:53:31 +0200

    comment some line

--- backend/tests/user.test.ts
+++ backend/tests/user.test.ts
@@ -1,4 +1,4 @@
 import { User } from './../src/models/userModel';
-import supertest from 'supertest'
+// import supertest from 'supertest'
 import initApp from '../src/app'
 import mongoose from 'mongoose';
