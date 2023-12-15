f1466842ddc244cb28598ba7dd4d430acabeda67
unknown <shakedabdu50@gmail.com>
2023-12-15 13:56:39 +0200

    add comments delete

--- backend/src/routes/commentRutes.ts
+++ backend/src/routes/commentRutes.ts
@@ -5,5 +5,6 @@ const commentRouter = Router()
 
 commentRouter.get('/', commentController.getAll.bind(commentController))
 commentRouter.post('/', commentController.create.bind(commentController))
+commentRouter.delete('/:id', commentController.deleteById.bind(commentController))
 
 export = commentRouter
