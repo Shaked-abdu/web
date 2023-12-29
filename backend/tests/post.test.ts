478e1782edb890464aab01184cd5bae94ad6fd59
Shaked <shakedabdu50@gmail.com>
2023-12-29 15:55:03 +0200

    remove old tests

--- backend/tests/post.test.ts
+++ backend/tests/post.test.ts
@@ -19,7 +19,8 @@ beforeAll(async () => {
   await postModel.deleteMany();
   await userModel.deleteMany({ email: user.email });
   user._id = (await request(app).post("/auth/register").send(user)).body._id;
-  accessToken = (await request(app).post("/auth/login").send(user)).body.accessToken;
+  accessToken = (await request(app).post("/auth/login").send(user)).body
+    .accessToken;
 });
 
 afterAll(async () => {
@@ -39,58 +40,56 @@ describe("post tests", () => {
       .set("Authorization", `Bearer ${accessToken}`)
       .send(post);
     expect(res.status).toBe(StatusCodes.CREATED);
-    // doctorPost._id = res.body._id;
-    // expect(res.body.owner).toMatchObject(user._id);
   });
-  test("Get post", async () => {
-    const res = await request(app).get("/posts/123");
-    expect(res.status).toBe(StatusCodes.OK);
-    expect(res.body).toMatchObject(post);
-  });
-  test("Update post", async () => {
-    const res = await request(app)
-      .put("/posts/123")
-      .send({
-        title: "title",
-        content: "content",
-        doctorId: "Shalom",
-        _id: "123",
-      })
-      .set("Authorization", `Bearer ${accessToken}`);
-    expect(res.status).toBe(StatusCodes.OK);
-    expect(res.body).toMatchObject(post);
-  });
-  test("get post comments", async () => {
-    const res = await request(app).get("/posts/123/comments");
-    expect(res.status).toBe(StatusCodes.OK);
-    expect(res.body).toMatchObject([]);
-  });
-  test("update post with invalid doctorId", async () => {
-    const res = await request(app)
-      .put("/posts/123")
-      .send({
-        title: "title",
-        content: "content",
-        doctorId: "Shalom2",
-        _id: "123",
-      })
-      .set("Authorization", `Bearer ${accessToken}`);
-    expect(res.status).toBe(StatusCodes.BAD_REQUEST);
-  });
-  test("Create post with non existing doctorId", async () => {
-    const res = await request(app)
-      .post("/posts")
-      .send({
-        title: "title",
-        content: "content",
-        doctorId: "Shalom2",
-        _id: "123",
-      })
-      .set("Authorization", `Bearer ${accessToken}`);
-    expect(res.status).toBe(StatusCodes.BAD_REQUEST);
-  });
-  test("Get post comments of non existing post", async () => {
-    const res = await request(app).get("/posts/1234/comments");
-    expect(res.status).toBe(StatusCodes.BAD_REQUEST);
-  });
+  // test("Get post", async () => {
+  //   const res = await request(app).get("/posts/123");
+  //   expect(res.status).toBe(StatusCodes.OK);
+  //   expect(res.body).toMatchObject(post);
+  // });
+  // test("Update post", async () => {
+  //   const res = await request(app)
+  //     .put("/posts/123")
+  //     .send({
+  //       title: "title",
+  //       content: "content",
+  //       doctorId: "Shalom",
+  //       _id: "123",
+  //     })
+  //     .set("Authorization", `Bearer ${accessToken}`);
+  //   expect(res.status).toBe(StatusCodes.OK);
+  //   expect(res.body).toMatchObject(post);
+  // });
+  // test("get post comments", async () => {
+  //   const res = await request(app).get("/posts/123/comments");
+  //   expect(res.status).toBe(StatusCodes.OK);
+  //   expect(res.body).toMatchObject([]);
+  // });
+  // test("update post with invalid doctorId", async () => {
+  //   const res = await request(app)
+  //     .put("/posts/123")
+  //     .send({
+  //       title: "title",
+  //       content: "content",
+  //       doctorId: "Shalom2",
+  //       _id: "123",
+  //     })
+  //     .set("Authorization", `Bearer ${accessToken}`);
+  //   expect(res.status).toBe(StatusCodes.BAD_REQUEST);
+  // });
+  // test("Create post with non existing doctorId", async () => {
+  //   const res = await request(app)
+  //     .post("/posts")
+  //     .send({
+  //       title: "title",
+  //       content: "content",
+  //       doctorId: "Shalom2",
+  //       _id: "123",
+  //     })
+  //     .set("Authorization", `Bearer ${accessToken}`);
+  //   expect(res.status).toBe(StatusCodes.BAD_REQUEST);
+  // });
+  // test("Get post comments of non existing post", async () => {
+  //   const res = await request(app).get("/posts/1234/comments");
+  //   expect(res.status).toBe(StatusCodes.BAD_REQUEST);
+  // });
 });
