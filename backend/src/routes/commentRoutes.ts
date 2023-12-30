98910ac33fd394092bfd6148296c6d5f88dff181
Shaked <shakedabdu50@gmail.com>
2023-12-30 18:31:56 +0200

    Swagger

--- backend/src/routes/commentRoutes.ts
+++ backend/src/routes/commentRoutes.ts
@@ -1,11 +1,76 @@
-import { Router } from 'express'
-import commentController from '../controllers/commentController'
-import authMiddleware from '../common/authMiddleware'
+import { Router } from "express";
+import commentController from "../controllers/commentController";
+import authMiddleware from "../common/authMiddleware";
 
-const commentRouter = Router()
+const commentRouter = Router();
 
-commentRouter.get('/post/:id', authMiddleware, commentController.getByPostId.bind(commentController))
-commentRouter.post('/', authMiddleware, commentController.post.bind(commentController))
-commentRouter.delete('/:id', authMiddleware,commentController.deleteById.bind(commentController))
+/**
+ * @swagger
+ * /post/{id}:
+ *   get:
+ *     tags:
+ *       - Comments
+ *     summary: Get comments by post ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: ID of the post to get comments for
+ *     responses:
+ *       200:
+ *         description: Successful operation
+ */
+commentRouter.get(
+  "/post/:id",
+  authMiddleware,
+  commentController.getByPostId.bind(commentController)
+);
+/**
+ * @swagger
+ * /:
+ *   post:
+ *     tags:
+ *       - Comments
+ *     summary: Post a new comment
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Comment'
+ *     responses:
+ *       201:
+ *         description: Created
+ */
+commentRouter.post(
+  "/",
+  authMiddleware,
+  commentController.post.bind(commentController)
+);
+/**
+ * @swagger
+ * /{id}:
+ *   delete:
+ *     tags:
+ *       - Comments
+ *     summary: Delete a comment by ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: ID of the comment to delete
+ *     responses:
+ *       200:
+ *         description: Deleted
+ */
+commentRouter.delete(
+  "/:id",
+  authMiddleware,
+  commentController.deleteById.bind(commentController)
+);
 
-export = commentRouter
+export = commentRouter;
--- backend/src/routes/postRutes.ts
+++ backend/src/routes/postRutes.ts
@@ -3,10 +3,103 @@ import postController from "../controllers/postsController";
 import authMiddleware from "../common/authMiddleware";
 
 const postRouter = Router();
+/**
+ * @swagger
+ * /posts:
+ *   get:
+ *     tags:
+ *       - Posts
+ *     summary: Get all posts
+ *     responses:
+ *       200:
+ *         description: Successful operation
+ *       500:
+ *         description: Server error
+ */
 postRouter.get("/", authMiddleware, postController.getAll.bind(postController));
+/**
+ * @swagger
+ * /posts/{id}:
+ *   get:
+ *     tags:
+ *       - Posts
+ *     summary: Get a post by ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: ID of the post to get
+ *     responses:
+ *       200:
+ *         description: Successful operation
+ *       404:
+ *        description: Post not found
+ *       500:
+ *        description: Server error
+ */
 postRouter.get("/:id", authMiddleware, postController.getById.bind(postController));
+/**
+ * @swagger
+ * /comments/user/{id}:
+ *   get:
+ *     tags:
+ *       - Posts
+ *     summary: Get posts by user ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: ID of the user to get posts for
+ *     responses:
+ *       200:
+ *         description: Successful operation
+ *       500:
+ *         description: Server error
+ */
 postRouter.get("/user/:id", authMiddleware, postController.getByUserId.bind(postController));
+
+/**
+ * @swagger
+ * /posts:
+ *   post:
+ *     tags:
+ *       - Posts
+ *     summary: Create a new post
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Post'
+ *     responses:
+ *       201:
+ *         description: Created
+ *       500:
+ *         description: Server error
+ */
 postRouter.post("/", authMiddleware, postController.post.bind(postController));
+/**
+ * @swagger
+ * /posts/{id}:
+ *   delete:
+ *     tags:
+ *       - Posts
+ *     summary: Delete a post by ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: ID of the post to delete
+ *     responses:
+ *       200:
+ *         description: Deleted
+ */
 postRouter.delete("/:id", authMiddleware, postController.deleteById.bind(postController));
 
 export = postRouter;
