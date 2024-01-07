5b827c1b662c181cb85d0836ac2d28a98d277be4
Shaked <shakedabdu50@gmail.com>
2024-01-07 23:15:19 +0200

    add google auth

--- backend/src/routes/googleRoutes.ts
+++ backend/src/routes/googleRoutes.ts
@@ -1,4 +1,3 @@
-import { session } from 'express-session';
 import passport from "passport";
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 import { Request, Response, Router } from "express";
