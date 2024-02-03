5444f6408262316c4e5f2504af43aea16d86ccbd
Shaked <shakedabdu50@gmail.com>
2024-02-03 19:32:22 +0200

    vite https

--- frontend/front/vite.config.ts
+++ frontend/front/vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { readFileSync } from 'fs';
 
-const privateKey = readFileSync('path/to/key.pem');
-const certificate = readFileSync('path/to/cert.pem');
+const privateKey = readFileSync('../../../vite-key.pem');
+const certificate = readFileSync('../../../vite-cert.pem');
 
 // https://vitejs.dev/config/
 export default defineConfig({
