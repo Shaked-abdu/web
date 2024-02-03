d2b9a3a07cfa604cbdcc8eedb24da1fcca88ec26
Shaked <shakedabdu50@gmail.com>
2024-02-03 21:49:28 +0200

    remove https

--- frontend/front/vite.config.ts
+++ frontend/front/vite.config.ts
@@ -1,17 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { readFileSync } from 'fs';
 
-const privateKey = readFileSync('../../../vite-key.pem');
-const certificate = readFileSync('../../../vite-cert.pem');
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    https: {
-      key: privateKey,
-      cert: certificate,
-    },
-  },
-});
+})
