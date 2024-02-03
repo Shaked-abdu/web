33e8b0bae5ed2b7ccf9911a82629aa53ff61f89c
Shaked <shakedabdu50@gmail.com>
2024-02-03 22:05:00 +0200

    fix server url

--- frontend/front/config.tsx
+++ frontend/front/config.tsx
@@ -2,7 +2,7 @@ const second = 1000;
 const minute = 60 * second;
 const hour = 60 * minute;
 
-const API_URL = "https://193.106.55.164:80";
+const API_URL = "https://193.106.55.164:443";
 const ACCESS_TOKEN_EXPIRES_IN = hour - 10 * minute;
 
 export { API_URL, ACCESS_TOKEN_EXPIRES_IN };
